e0be386e2a42f3d3549133b90180a6914528ebec
Mohsen Mirzania <afshinpaydar@gmail.com>
2019-04-10 11:24:10 +0430

    Add ELK to openshift

--- CICD.sh
+++ CICD.sh
@@ -1,7 +1,35 @@
+git clone https://github.com/openshift/origin.git
+cd origin
+vim examples/jenkins/jenkins-persistent-template.json
+/"kind": "PersistentVolumeClaim",
 
-oc get   templates -n openshift
-oc edit templates jenkins-persistent -n openshift
-/kind: PersistentVolumeClaim
+#            "apiVersion": "v1",
+#            "kind": "PersistentVolumeClaim",
+#            "metadata": {
+#                "name": "${JENKINS_SERVICE_NAME}"
+#            },
+#            "spec": {
+#                "accessModes": [
+#                    "ReadWriteOnce"
+#                ],
+#                "resources": {
+#                    "requests": {
+#                        "storage": "${VOLUME_CAPACITY}"
+#                    }
+#                },
+#                "storageClassName": "glusterfs-storage"
+
+#oc delete templates jenkins-persistent -n openshift
+oc apply -f examples/jenkins/jenkins-persistent-template.json -n openshift
+#Check 
+oc edit templates jenkins-persistent -n openshift -o json
+/"kind": "PersistentVolumeClaim",
+
+
+
+#oc get   templates -n openshift
+#oc edit templates jenkins-persistent -n openshift
+#/kind: PersistentVolumeClaim
 #Add '    storageClassName: glusterfs-storage' below resources:
 #- apiVersion: v1
 #  kind: PersistentVolumeClaim
@@ -15,18 +43,19 @@ oc edit templates jenkins-persistent -n openshift
 #        storage: ${VOLUME_CAPACITY}
 #    storageClassName: glusterfs-storage
 
-:x
+#:x
 
 oc new-project jenkins-persistent --description="CI/CD project" --display-name="Jenkins"
 
 oc new-app -e \
-    OPENSHIFT_ENABLE_OAUTH=true \
-    JENKINS_PASSWORD=<Password> \
-    INSTALL_PLUGINS=tfs,slack\
+    JENKINS_PASSWORD=admin1234 \
+    INSTALL_PLUGINS=tfs\
     OVERRIDE_PV_PLUGINS_WITH_IMAGE_PLUGINS=true \
     jenkins-persistent
 #check environment that passed to pod!
 #oc exec jenkins-1-7ktbb -it -- env | grep INSTALL_PLUGINS
+#oc exec jenkins-1-t45wx -it -- env | grep MEMORY_LIMIT
+#oc exec jenkins-1-t45wx -it -- env | grep MEMORY_REQUEST
 
 oc adm policy add-role-to-user admin admin -n jenkins-persistent
 
---
+++ ELK/ELK.sh
@@ -0,0 +1,71 @@
+#On VPN Server:
+docker pull docker.elastic.co/elasticsearch/elasticsearch-oss:6.4.3
+docker pull docker.elastic.co/kibana/kibana-oss:6.4.3
+docker save docker.elastic.co/elasticsearch/elasticsearch-oss:6.4.3 -o /tmp/elasticsearch-oss.tar
+docker save docker.elastic.co/kibana/kibana-oss:6.4.3 -o /tmp/kibana-oss.tar
+scp /tmp/elasticsearch-oss.tar soshya-openshift-node1:/tmp/ 
+scp /tmp/elasticsearch-oss.tar soshya-openshift-node2:/tmp/ 
+scp /tmp/elasticsearch-oss.tar soshya-openshift-node3:/tmp/ 
+
+scp /tmp/kibana-oss.tar soshya-openshift-node1:/tmp/ 
+scp /tmp/kibana-oss.tar soshya-openshift-node2:/tmp/ 
+scp /tmp/kibana-oss.tar soshya-openshift-node3:/tmp/
+#On All Nodes
+docker load -i /tmp/elasticsearch-oss.tar
+docker load -i /tmp/kibana-oss.tar
+#On Master Node
+oc new-project elasticsearch --display-name="ELK Project"
+oc adm policy add-role-to-user admin admin -n elasticsearch
+oc create serviceaccount -n privilegeduser
+oc adm policy add-scc-to-user privileged -nelasticsearch -z privilegeduser
+oc adm policy add-scc-to-user privileged -nelasticsearch -z default
+oc create -f elastic-svc.yaml
+oc create -f elastic-app.yaml
+
+#Check
+oc get svc
+oc get pod
+oc port-forward es-cluster-0 9200:9200 &
+curl localhost:9200
+#Handling connection for 9200
+# {
+#   "name" : "es-cluster-0",
+#   "cluster_name" : "px-elk-demo",
+#   "cluster_uuid" : "DfyBasqmR_OtIGoEiEChKg",
+#   "version" : {
+#     "number" : "6.4.3",
+#     "build_flavor" : "oss",
+#     "build_type" : "tar",
+#     "build_hash" : "fe40335",
+#     "build_date" : "2018-10-30T23:17:19.084789Z",
+#     "build_snapshot" : false,
+#     "lucene_version" : "7.4.0",
+#     "minimum_wire_compatibility_version" : "5.6.0",
+#     "minimum_index_compatibility_version" : "5.0.0"
+#   },
+#   "tagline" : "You Know, for Search"
+# }
+
+#need jq 'yum install jq'
+curl -s localhost:9200/_nodes | jq ._nodes
+
+fg
+#Cntl+C
+
+
+#KIBANA
+oc create -f kibana-svc.yaml
+oc create -f kibana-app.yaml
+oc expose svc kibana
+
+#LOGSTASH
+#Later must save all config to config file and edit logstash.yaml to mount configmap instead !!!
+#oc create configmap logstash-config --from-file=config/logstash.conf
+oc create -f logstash.yaml
+oc expose svc logstash
+oc get route
+
+
+#Test from local host (import log to logstash):
+#wget https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_json_logs/nginx_json_logs
+#curl -v -H "content-type: application/json" http://logstash-elasticsearch.apps.soshyant.local -d @'/tmp/nginx_json_logs'
