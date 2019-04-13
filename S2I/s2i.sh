14c9065fbf29307d0dc255afeb027a2784053c46
Mohsen Mirzania <afshinpaydar@gmail.com>
2019-04-13 14:18:32 +0430

    s2i progress...

--- S2I/s2i.sh
+++ S2I/s2i.sh
@@ -4,14 +4,19 @@ yum --enablerepo=centos-sclo-rh-testing install source-to-image
 docker pull registry.centos.org/dotnet/dotnet-21-centos7:latest
 
 
-s2i build https://github.com/redhat-developer/s2i-dotnetcore-ex registry.centos.org/dotnet/dotnet-21-centos7:latest mywebapp -r dotnetcore-2.1 -e DOTNET_STARTUP_PROJECT=app -p always
+NuGet.Config
 
-#Test
-docker run --rm -p 8080:8080 mywebapp
+<configuration>
+    <packageSources>
+	<add key="repo" value="http://192.168.168.34/repository/nuget-hosted/" />
+    </packageSources>
+</configuration>
 
 
-s2i build https://github.com/afshinpaydar/test.git registry.centos.org/dotnet/dotnet-21-centos7:latest afshin  -e DOTNET_STARTUP_PROJECT=app/SadTahli.WebApi/src/SadTahli.WebApi.csproj -p always
 
-#https://github.com/redhat-developer/s2i-dotnetcore.git
+s2i build https://github.com/afshinpaydar/test.git registry.centos.org/dotnet/dotnet-21-centos7:latest afshin  -e DOTNET_STARTUP_PROJECT=SadTahli.WebApi/src/SadTahli.WebApi.csproj -e DOTNET_PUBLISH=/opt/app-root/app/  -e DOTNET_ASSEMBLY_NAME=Soshyant.Repo.SadTahli.WebApi -p always
 
-#scp /home/mohsen/Documents/Soshyant/100Tahlil.zip soshya-openshift-master1:/root/s2i-dotnetcore/2.1/
+
+
+
+oc new-app registry.centos.org/dotnet/dotnet-21-centos7:latest~https://github.com/afshinpaydar/test.git  -e DOTNET_STARTUP_PROJECT=SadTahli.WebApi/src/SadTahli.WebApi.csproj -e DOTNET_PUBLISH=/opt/app-root/app/  -e DOTNET_ASSEMBLY_NAME=Soshyant.Repo.SadTahli.WebApi
