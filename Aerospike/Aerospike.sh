ee463e8130051ecdf76b7282864e77c05611ac74
Mohsen Mirzania <afshinpaydar@gmail.com>
2019-04-17 10:58:30 +0430

    Add NATS and change aerospike

--- Aerospike/Aerospike.sh
+++ Aerospike/Aerospike.sh
@@ -24,8 +24,14 @@ G
 
 :x
 
+oc new-project  aerospike-operator
+oc adm policy add-scc-to-user anyuid -z default
 
 oc create -f docs/examples/00-prereqs.yml
 oc adm policy add-role-to-user admin admin -n aerospike-operator #It must show "aerospike-operator" project in openshift panel for admin user!
 oc create -f docs/examples/10-aerospike-operator.yml
 oc create -f docs/examples/20-aerospike-cluster.yml
+
+#ALL Nodes
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 3000 -j ACCEPT
+
