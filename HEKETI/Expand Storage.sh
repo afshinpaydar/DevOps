da4a549c982b2e9a0b0538400d25a1efb2ebc8b7
Afshin Paydar <afshinpaydar@gmail.com>
2019-05-25 10:04:50 +0430

    Update Expand Storage.sh

--- HEKETI/Expand Storage.sh
+++ HEKETI/Expand Storage.sh
@@ -3,13 +3,24 @@ oc get pod
 oc describe po <heketi_pod>
 oc describe po heketi-storage-1-qx925
 
+oc describe po heketi-storage-1-gsj9w |  grep HEKETI_ADMIN_KEY
+      HEKETI_ADMIN_KEY:                0x6LamEHYAAgN62Ka3l3HTh6TwRLKCSYx0F/rv23yUU=
+
 oc rsh <heketi_pod> heketi-cli -s http://localhost:8080 --user admin --secret <admin_key> topology info
 oc rsh heketi-storage-1-qx925  heketi-cli -s http://localhost:8080 --user admin --secret b4Tbnht6EfxjNQ7TpHcC4fIkftaga8Wt4f4ce3cMbuY= topology info
+
 oc rsh heketi-storage-1-qx925  heketi-cli -s http://localhost:8080 --user admin --secret b4Tbnht6EfxjNQ7TpHcC4fIkftaga8Wt4f4ce3cMbuY= device add --help
 
-oc rsh heketi-storage-1-ff995  heketi-cli -s http://localhost:8080 --user admin --secret b4Tbnht6EfxjNQ7TpHcC4fIkftaga8Wt4f4ce3cMbuY= topology info | grep 'Node Id'
-	Node Id: 62677c42440b4c1435b49ffc07b58ad1
-	Node Id: 7afa8bbe1539575c3695f63b8f6c3918
-	Node Id: 8646350701222331fad017fecbc89345
+oc rsh heketi-storage-1-gsj9w  heketi-cli -s http://localhost:8080 --user admin --secret 0x6LamEHYAAgN62Ka3l3HTh6TwRLKCSYx0F/rv23yUU= topology info | grep 'Node Id'
+	Node Id: 1d22a43008f3742d9a3b20edcbf23b50
+	Node Id: 4c09f7bec67805198e06023827576782
+	Node Id: 7ed4159c956989d7bbd15a2dd3ae6a53
 
-oc rsh heketi-storage-1-ff995  heketi-cli -s http://localhost:8080 --user admin --secret b4Tbnht6EfxjNQ7TpHcC4fIkftaga8Wt4f4ce3cMbuY= device add --name=/dev/sdb1 --node=62677c42440b4c1435b49ffc07b58ad1
+oc rsh heketi-storage-1-gsj9w  heketi-cli -s http://localhost:8080 --user admin --secret 0x6LamEHYAAgN62Ka3l3HTh6TwRLKCSYx0F/rv23yUU= device add --name=/dev/sdd --node=1d22a43008f3742d9a3b20edcbf23b50
+Device added successfully
+
+oc rsh heketi-storage-1-gsj9w  heketi-cli -s http://localhost:8080 --user admin --secret 0x6LamEHYAAgN62Ka3l3HTh6TwRLKCSYx0F/rv23yUU= device add --name=/dev/sdd --node=4c09f7bec67805198e06023827576782
+Device added successfully
+
+oc rsh heketi-storage-1-gsj9w  heketi-cli -s http://localhost:8080 --user admin --secret 0x6LamEHYAAgN62Ka3l3HTh6TwRLKCSYx0F/rv23yUU= device add --name=/dev/sdd --node=7ed4159c956989d7bbd15a2dd3ae6a53
+Device added successfully
